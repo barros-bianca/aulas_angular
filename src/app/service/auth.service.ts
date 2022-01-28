53e4505bb18c0337a5e464b71e0d7202475b32c1
Bianca Barros <biancabarrosufrj@gmail.com>
2022-01-28 17:59:28 -0300

    [Task: Componente inicio sem cosumo]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
 import { UserModel } from '../model/UserModel';
 
@@ -20,6 +21,14 @@ export class AuthService {
 
   cadastrar(userModel: UserModel): Observable<UserModel>{
     return this.http.post<UserModel>('http://localhost:8080/usuario/cadastro', userModel)
+  }
+
+  logado(){
+   let ok: boolean = false 
+     if (environment.token != ''){
+       ok = true
+     }
+   return ok
   }
 }
 
