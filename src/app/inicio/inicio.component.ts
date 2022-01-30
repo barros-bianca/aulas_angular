f856e9984a2bd2e85c9f951980a7be76bcc6c140
Bianca Barros <biancabarrosufrj@gmail.com>
2022-01-30 16:30:13 -0300

    [Task: Construção de Cards]

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { UserModel } from '../model/UserModel';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,17 +14,65 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  listaPostagens: Postagem[]
+  postagem: Postagem = new Postagem()
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  id: number
+
+  userModel: UserModel = new UserModel()
+  idUser = environment.idUser
+
   constructor(
 
-    private router :Router
+    private router :Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
 
   ){ }
 
   ngOnInit(){
     
-    
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    this.getAllTemas()
+    this.getAllPostagens()
   }
 
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.id).subscribe((resp: Tema) =>{
+      this.tema = resp
+    })
+  }
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp
+    })
+  }
   
+  publicar(){
+
+    this.tema.idTema = this.id
+    this.postagem.tema = this.tema
+
+    this.userModel.idUser = this.idUser
+    this.postagem.user = this.userModel
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+      this.getAllPostagens()
+      
+    })
+  }
 
 }
